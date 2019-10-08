051d9a1b99aac10a0a2a9d3bfd749392be35dddf
Amalia Safira <amaliayuya@gmail.com>
2019-10-08 23:12:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -140,7 +140,6 @@ function create_response($text, $message)
             $b++;
         }
         $a = 1;
-        $tambahan = array();
         foreach ($total_array as $key => $value) {
             if ($a == 1) {
                 if ($value != 0) {
@@ -157,10 +156,10 @@ function create_response($text, $message)
                     $query = "SELECT * FROM wisata WHERE idwisata = '$key'";
                     $hasil = mysqli_query($conn,$query);
                     while($row = mysqli_fetch_assoc($hasil)){
-                        $tambahan[] = "Temuan Lainnya : ".$row["nama_wisata"]." , ".$row["informasi"];
+                        $tambahan .= "Temuan Lainnya : ".$row["nama_wisata"]." , ".$row["informasi"];
                     }
                 }else{
-                    $tambahan[] = "";
+                    $tambahan = "";
                 }
             }
             $a++;
